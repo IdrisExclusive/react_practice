4374d6000d56971b824d95ba18c0d027b0396074
Idris Ahmad <Idris.Ahmad@nnpcgroup.com>
2023-09-14 03:52:37 +0100

    created a button and a profile list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,147 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
+import { useState } from 'react';
 import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+type userType = {
+    name: string;
+    imageUrl: string;
+    attributes: Array<string>
+}
+
+type personType = {
+    id: number;
+    name: string;
+    profession: string;
+    accomplishment: string;
+    imageId: string;
+}
+
+const user: userType = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    attributes: ["Beautifully looking", "Calming countenance", "eloquent in speech"]
+}
+
+const people: Array<personType> = [{
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A'
+  }, {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa'
+  }, {
+    id: 2,
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+    accomplishment: 'electromagnetism theory',
+    imageId: 'bE7W1ji'
+  }, {
+    id: 3,
+    name: 'Percy Lavon Julian',
+    profession: 'chemist',
+    accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+    imageId: 'IOjWm71'
+  }, {
+    id: 4,
+    name: 'Subrahmanyan Chandrasekhar',
+    profession: 'astrophysicist',
+    accomplishment: 'white dwarf star mass calculations',
+    imageId: 'lrWQx8l'
+  }];
+  
+
+function getImageUrl(person: personType) {
+    return (
+      'https://i.imgur.com/' +
+      person.imageId +
+      's.jpg'
+    );
+  }
+
+function Button({text, onClick}: {text: string, onClick: () => void}) {
+    return(
+        <button className=" group transition-all w-56 h-16 py-4 px-6 rounded-full 
+                            bg-gradient-to-r from-sky-500 to-indigo-500 shadow-lg shadow-sky-200
+                            hover:bg-gradient-to-l hover:from-rose-500 hover:to-yellow-500
+                            hover:shadow-2xl hover:shadow-pink-300
+                            hover:-translate-y-1 hover:scale-110 duration-500 delay-50"
+                            onClick={onClick}>
+            <span className="font-sans font-bold text-white text-2xl">{text}</span>
+            
+        </button>
+    )
+}
+
+function Heading({text}: {text: string}) {
+    return(
+        <div className="text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                {text}
         </div>
-      </div>
+    )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+function List() {
+    const listItems: Array<any> = people.map(person =>
+      <li key={person.id} className='flex gap-8 items-center'>
+        <img
+          src={getImageUrl(person)}
+          alt={person.name}
+          className='rounded-full'
         />
-      </div>
+        <p>
+          <b>{person.name}:</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+        </p>
+      </li>
+    );
+    return (
+      <ul className='flex flex-col gap-8'>{listItems}</ul>
+    );
+  }
+  
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function Profile({name, imageUrl, attributes}: userType) {
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const attributesList: Array<any> = attributes.map(attribute =>
+            <li key = {attribute} >
+                <h1 className='font-semibold'>{attribute}</h1>
+            </li>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    )
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <div className='flex flex-row gap-8 items-center'>
+            <img 
+                src= {imageUrl}
+                alt={`profile picture of ${name}`}
+                className='w-40 h-40 rounded-full'
+            />
+            <ul className='list-disc'>
+                {attributesList}
+            </ul>
+        </div>
+    )
+}
+
+export default function Body({children}: any) {
+    const [clicked, setClicked] = useState<boolean>(false);
+
+    const backgroundColor: string = 'bg-white'; 
+
+    return(
+        <div className= {`transition-all ${clicked? 'bg-slate-700': 'bg-sky-200'} w-full h-fit flex flex-col items-center gap-8 p-8 duration-500 ease-linear`}>
+            <Heading text = {user.name} />  
+            <Profile name={user.name} imageUrl={user.imageUrl} attributes={user.attributes} />           
+            <List />
+            <Button text = "Click Me" onClick = {()=>setClicked(!clicked)}/> 
+            {children}       
+        </div>
+    )
 }
---
+++ src/app/pages/react_tests.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+    return(
+        <button className="w-8 h-8 p-4 bg-gradient-to-r from-sky-500 to-indigo-500" >
+            <span className="font-sans font-bold text-white text-2xl">Click Me!</span>
+        </button>
+    )
+}
